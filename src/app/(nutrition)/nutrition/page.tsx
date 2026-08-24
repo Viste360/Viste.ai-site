e4326535b07a134b0afa5151ed25396be4ccb116
Dustdeep <yon.wallace@viste.ai>
2026-08-24 15:20:33 +0200

    Revert "feat: add private nutrition coach"
    This reverts commit cbb19fee480b87135ad8a769b74511f4a1591c66.

--- src/app/(nutrition)/nutrition/page.tsx
+++
@@ -1,5 +0,0 @@
-import { NutritionApp } from "@/components/nutrition/nutrition-app";
-
-export default function NutritionPage() {
-  return <NutritionApp locale="en" />;
-}
--- src/app/(nutrition-es)/es/nutricion/page.tsx
+++
@@ -1,5 +0,0 @@
-import { NutritionApp } from "@/components/nutrition/nutrition-app";
-
-export default function NutritionSpanishPage() {
-  return <NutritionApp locale="es" />;
-}
